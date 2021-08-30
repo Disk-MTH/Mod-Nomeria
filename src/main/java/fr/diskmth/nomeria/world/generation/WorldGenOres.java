31f8f95ae898f71c0597879bb52fc0cc306b56ac
Disk_MTH <gillet.mat@free.fr>
2021-08-30 11:02:27 +0200

    génération correcte

--- src/main/java/fr/diskmth/nomeria/world/generation/WorldGenOres.java
+++ src/main/java/fr/diskmth/nomeria/world/generation/WorldGenOres.java
@@ -22,10 +22,10 @@ public class WorldGenOres implements IWorldGenerator
 
     public WorldGenOres()
     {
-        TOPAZE_ORE = new WorldGenMinable(BlocksInit.TOPAZE_ORE.getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE));
-        AZURITE_ORE = new WorldGenMinable(BlocksInit.AZURITE_ORE.getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE));
-        MERCURE_ORE = new WorldGenMinable(BlocksInit.MERCURE_ORE.getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE));
-        RANDOM_ORE = new WorldGenMinable(BlocksInit.RANDOM_ORE.getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE));
+        TOPAZE_ORE = new WorldGenMinable(BlocksInit.TOPAZE_ORE.getDefaultState(), 10, BlockMatcher.forBlock(Blocks.STONE));
+        AZURITE_ORE = new WorldGenMinable(BlocksInit.AZURITE_ORE.getDefaultState(), 8, BlockMatcher.forBlock(Blocks.STONE));
+        MERCURE_ORE = new WorldGenMinable(BlocksInit.MERCURE_ORE.getDefaultState(), 5, BlockMatcher.forBlock(Blocks.STONE));
+        RANDOM_ORE = new WorldGenMinable(BlocksInit.RANDOM_ORE.getDefaultState(), 2, BlockMatcher.forBlock(Blocks.STONE));
     }
 
     @Override
@@ -34,15 +34,15 @@ public class WorldGenOres implements IWorldGenerator
         switch(world.provider.getDimension())
         {
             case 0:
-                runGenerator(TOPAZE_ORE, world, random, chunkX, chunkZ, 9, 2, 15);
-                runGenerator(AZURITE_ORE, world, random, chunkX, chunkZ, 7, 2, 15);
-                runGenerator(MERCURE_ORE, world, random, chunkX, chunkZ, 5, 2, 15);
-                runGenerator(RANDOM_ORE, world, random, chunkX, chunkZ, 2, 2, 15);
+                runGenerator(TOPAZE_ORE, world, random, chunkX, chunkZ, 2.25, 2, 15);
+                runGenerator(AZURITE_ORE, world, random, chunkX, chunkZ, 1.4, 2, 15);
+                runGenerator(MERCURE_ORE, world, random, chunkX, chunkZ, 0.7, 2, 15);
+                runGenerator(RANDOM_ORE, world, random, chunkX, chunkZ, 0.6, 2, 15);
                 break;
         }
     }
 
-    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight)
+    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, double chance, int minHeight, int maxHeight)
     {
         if(minHeight > maxHeight || minHeight < 0 || maxHeight > 256)
             throw new IllegalArgumentException("Ore generated out of bounds");
