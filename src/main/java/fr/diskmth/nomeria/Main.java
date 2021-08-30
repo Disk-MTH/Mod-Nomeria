25ba314eaba85d527a9348dd494323f71e290679
Disk_MTH <gillet.mat@free.fr>
2021-08-30 11:27:42 +0200

    joueur correct 2

--- src/main/java/fr/diskmth/nomeria/Main.java
+++ src/main/java/fr/diskmth/nomeria/Main.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import fr.diskmth.nomeria.utils.References;
 import fr.diskmth.nomeria.utils.handlers.RegistryHandler;
 import fr.diskmth.nomeria.utils.proxy.CommonProxy;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -34,15 +33,7 @@ public class Main
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent e) throws Exception
-    {
-        if(e.getSide().isClient())
-        {
-            if(!Minecraft.getMinecraft().mcDataDir.getPath().contains(".Nomeria"))
-            {
-                throw(new Exception("Mauvais launcher détecté, veuillez passé par le launcher officiel de Noméria"));
-            }
-        }
-        
+    {       
         logger = e.getModLog();
         proxy.preInit();
         proxy.init();
