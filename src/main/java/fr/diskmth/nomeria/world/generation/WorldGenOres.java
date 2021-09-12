54703960e38afe891e603134a908612f4e55d585
Disk_MTH <gillet.mat@free.fr>
2021-09-12 10:46:56 +0200

    maj

--- src/main/java/fr/diskmth/nomeria/world/generation/WorldGenOres.java
+++ src/main/java/fr/diskmth/nomeria/world/generation/WorldGenOres.java
@@ -15,34 +15,47 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 
 public class WorldGenOres implements IWorldGenerator
 {
-    private WorldGenerator TOPAZE_ORE = (WorldGenerator)new WorldGenMinable(BlocksInit.TOPAZE_ORE.getDefaultState(), 10, BlockMatcher.forBlock(Blocks.STONE));
-    private WorldGenerator AZURITE_ORE = (WorldGenerator)new WorldGenMinable(BlocksInit.AZURITE_ORE.getDefaultState(), 8, BlockMatcher.forBlock(Blocks.STONE));
-    private WorldGenerator MERCURE_ORE = (WorldGenerator)new WorldGenMinable(BlocksInit.MERCURE_ORE.getDefaultState(), 5, BlockMatcher.forBlock(Blocks.STONE));
-    private WorldGenerator RANDOM_ORE = (WorldGenerator)new WorldGenMinable(BlocksInit.RANDOM_ORE.getDefaultState(), 2, BlockMatcher.forBlock(Blocks.STONE));
+    private final WorldGenerator TOPAZE_ORE;
+    private final WorldGenerator AZURITE_ORE;
+    private final WorldGenerator MERCURE_ORE;
+    private final WorldGenerator RANDOM_ORE;
 
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+    public WorldGenOres()
+    {
+        TOPAZE_ORE = new WorldGenMinable(BlocksInit.TOPAZE_ORE.getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
+        AZURITE_ORE = new WorldGenMinable(BlocksInit.AZURITE_ORE.getDefaultState(), 5, BlockMatcher.forBlock(Blocks.STONE));
+        MERCURE_ORE = new WorldGenMinable(BlocksInit.MERCURE_ORE.getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE));
+        RANDOM_ORE = new WorldGenMinable(BlocksInit.RANDOM_ORE.getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+    }
+
+    @Override public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
     {
         switch(world.provider.getDimension())
         {
             case 0:
-                runGenerator(this.TOPAZE_ORE, world, random, chunkX, chunkZ, 2.25D, 2, 15);
-                runGenerator(this.AZURITE_ORE, world, random, chunkX, chunkZ, 1.4D, 2, 15);
-                runGenerator(this.MERCURE_ORE, world, random, chunkX, chunkZ, 0.7D, 2, 15);
-                runGenerator(this.RANDOM_ORE, world, random, chunkX, chunkZ, 0.6D, 2, 15);
+                runGenerator(TOPAZE_ORE, world, random, chunkX, chunkZ, 1.75F, 2, 15);
+                runGenerator(AZURITE_ORE, world, random, chunkX, chunkZ, 1F, 2, 15);
+                runGenerator(MERCURE_ORE, world, random, chunkX, chunkZ, 0.5F, 2, 15);
+                runGenerator(RANDOM_ORE, world, random, chunkX, chunkZ, 0.05F, 2, 15);
                 break;
         }
     }
 
-    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, double chance, int minHeight, int maxHeight)
+    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, float chance, int minHeight, int maxHeight)
     {
         if(minHeight > maxHeight || minHeight < 0 || maxHeight > 256)
+        {
             throw new IllegalArgumentException("Ore generated out of bounds");
+        }
+
         int heightDiff = maxHeight - minHeight + 1;
-        for(int i = 0; i < chance; i++)
+
+        for(float i = 0; i < chance; i++)
         {
             int x = chunkX * 16 + rand.nextInt(16);
             int y = minHeight + rand.nextInt(heightDiff);
             int z = chunkZ * 16 + rand.nextInt(16);
+
             gen.generate(world, rand, new BlockPos(x, y, z));
         }
     }
